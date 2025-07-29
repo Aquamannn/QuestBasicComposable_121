ed988bd4b6c3e894e184423a675f2f68d01809bb
Lenovo <9.3razzinayyuri@gmail.com>
2025-07-29 23:45:31 +0700

    4

--- app/src/main/java/com/example/activity2/LoginScreen.kt
+++ app/src/main/java/com/example/activity2/LoginScreen.kt
@@ -77,22 +77,6 @@ fun LoginScreen() {
 
         Spacer(modifier = Modifier.height(14.dp))
 
-        Surface(
-            shape = CircleShape,
-            modifier = Modifier
-                .size(350.dp)
-                .border(4.dp, Color.White, CircleShape),
-            tonalElevation = 6.dp
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.foto),
-                contentDescription = "Foto Profil",
-                modifier = Modifier
-                    .fillMaxSize()
-                    .clip(CircleShape),
-                alignment = Alignment.Center
-            )
-        }
     }
 }
 
