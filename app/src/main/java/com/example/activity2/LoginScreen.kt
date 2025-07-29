885e1eec898aabf365c6fdd06efa0e1b55852251
Lenovo <9.3razzinayyuri@gmail.com>
2025-07-29 22:58:02 +0700

    1

--- app/src/main/java/com/example/activity2/LoginScreen.kt
+++ app/src/main/java/com/example/activity2/LoginScreen.kt
@@ -17,4 +17,82 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(
+            text = "Login",
+            style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(2.dp))
+
+        Text(
+            text = "Ini adalah halaman login",
+            style = TextStyle(fontSize = 16.sp, color = Color.Gray)
+        )
+
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.logo_umy),
+            contentDescription = "Logo Universitas",
+            modifier = Modifier.size(300.dp)
+        )
+
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Text(
+            text = "Nama",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.Red
+        )
+
+        Text(
+            text = "Muhammad Razzin Ayyuri",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue
+        )
+
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Text(
+            text = "20230140121",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(14.dp))
+
+        Surface(
+            shape = CircleShape,
+            modifier = Modifier
+                .size(350.dp)
+                .border(4.dp, Color.White, CircleShape),
+            tonalElevation = 6.dp
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.foto),
+                contentDescription = "Foto Profil",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(CircleShape),
+                alignment = Alignment.Center
+            )
+        }
+    }
+}
 
